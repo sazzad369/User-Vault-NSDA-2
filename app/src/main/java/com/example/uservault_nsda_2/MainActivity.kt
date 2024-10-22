3736631620b50e743fc79f8592592c3b76581f46
sazzad369 <mdsazzad0369@gmail.com>
2024-10-22 13:29:32 +0600

    updated

---
+++ app/src/main/java/com/example/uservault_nsda_2/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.uservault_nsda_2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var listButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        listButton = findViewById(R.id.profileListBtn)
+
+        listButton.setOnClickListener {
+            // Start LoadingActivity instead of ProfileListActivity
+            val intent = Intent(this, LoadingActivity::class.java)
+            intent.putExtra("TARGET_ACTIVITY", "com.example.uservault_nsda_2.ProfileListActivity")
+
+            startActivity(intent)
+            finish() // Finish MainActivity so the user can't return to it
+        }
+    }
+}
